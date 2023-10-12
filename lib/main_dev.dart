b7f08dee84fff93c1baab2d5fd02f1e01f156914
Juan Insuasty <juan.insuasty@epik.com.co>
2023-10-12 12:17:30 -0500

    Build: Configuración para iOS y organización de ambientes

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,12 +8,14 @@ import 'package:todoapp/src/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyBiueWq8sc13nUmubAy_zU0yll_ob3jMf4",
-          appId: "1:366491493610:android:a53cba7d06b8b0c622a393",
-          messagingSenderId: "366491493610",
-          projectId: "todoapp-d2e0a"));
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: "AIzaSyBiueWq8sc13nUmubAy_zU0yll_ob3jMf4",
+              appId: "1:366491493610:android:a53cba7d06b8b0c622a393",
+              messagingSenderId: "366491493610",
+              projectId: "todoapp-d2e0a"))
+      : await Firebase.initializeApp();
 
   FlavorConfig(
     name: "DEV",
--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -5,7 +7,15 @@ import 'package:flutter_flavor/flutter_flavor.dart';
 import 'package:todoapp/src/app.dart';
 
 Future<void> main() async {
-  await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: "AIzaSyBiueWq8sc13nUmubAy_zU0yll_ob3jMf4",
+              appId: "1:366491493610:android:a53cba7d06b8b0c622a393",
+              messagingSenderId: "366491493610",
+              projectId: "todoapp-d2e0a"))
+      : await Firebase.initializeApp();
 
   FlavorConfig(
     name: "PROD",
